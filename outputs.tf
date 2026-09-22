bd69749155e23de0b8c3fb194181702bdd63cb98
Synapse-debug <morettid27@gmail.com>
2026-09-22 17:10:40 +0200

    new out

--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,6 @@
 output "aws_instance_public_dns" {
-  value = "http://${aws_instance.ec2.public_dns}:${var.http_port}"
+  description = "Public DNS riportato da EC2 (su Floci usa porta host 30000)"
+  value       = "http://${replace(aws_instance.ec2.public_dns, "localhost", "10.10.10.11")}:${var.http_port}"
 }
 
 output "vpc_id" {
@@ -10,5 +11,7 @@ output "public_subnet_id" {
   value = aws_subnet.test.id
 }
 
-
-    
+output "vero_ip_di_nginx" {
+  description = "Vero indirizzo per raggiungere Nginx esposto sulla macchina Floci"
+  value       = "http://10.10.10.11:30000"
+}
